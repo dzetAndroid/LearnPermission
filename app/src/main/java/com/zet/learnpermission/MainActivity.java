9665700d823d9b7041265c854c87c06e111daac2
haomengjie <qqduxingzhe@163.com>
2017-06-22 13:40:21 +0800

    little change

--- app/src/main/java/com/zet/learnpermission/MainActivity.java
+++ app/src/main/java/com/zet/learnpermission/MainActivity.java
@@ -12,8 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
     private TextView mTxtInfo;
@@ -21,55 +20,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mBtnTest;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        if (savedInstanceState == null)
-        {
+        if (savedInstanceState == null) {
             initView();
         }
     }
 
-    private void testPermission()
-    {
+    private void testPermission() {
         String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         // 检查权限
         int checkSelfPermission = ActivityCompat.checkSelfPermission(getApplicationContext(), permissions[0]);
 
-        if (PackageManager.PERMISSION_GRANTED != checkSelfPermission)
-        {
+        if (PackageManager.PERMISSION_GRANTED != checkSelfPermission) {
             Log.e(TAG, "onCreate: " + checkSelfPermission);
             mTxtInfo.setText("no");
 
             boolean b = ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0]);
-            if (b)
-            {
+            if (b) {
                 Toast.makeText(this, "需要权限以便 更好的服务", Toast.LENGTH_SHORT).show();
                 // 请求权限
                 ActivityCompat.requestPermissions(this, permissions, writeRequestCode);
             }
 
-        } else
-        {
+        } else {
             Log.e(TAG, "onCreate: " + checkSelfPermission);
             mTxtInfo.setText("yes");
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        switch (requestCode)
-        {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
             case writeRequestCode:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mTxtInfo.setText("get the permission");
-                } else
-                {
+                } else {
                     mTxtInfo.setText("oh! no!!!");
                 }
 
@@ -77,18 +65,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void initView()
-    {
+    private void initView() {
         mTxtInfo = (TextView) findViewById(R.id.mTxtInfo);
         mBtnTest = (Button) findViewById(R.id.mBtnTest);
         mBtnTest.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.mBtnTest:
                 testPermission();
                 break;
